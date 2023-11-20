634d60192571bf60bbd43d801e5fce4f2a6dbe0f
unknown <pingo15102002@gmail.com>
2023-11-20 17:53:43 +0100

    Removed all controllers except user controller, refactored to AllArgsConstructor

--- backend/LibraryManagementSystem/src/main/java/org/oss/LibraryManagementSystem/controllers/UserController.java
+++ backend/LibraryManagementSystem/src/main/java/org/oss/LibraryManagementSystem/controllers/UserController.java
@@ -1,17 +1,14 @@
 package org.oss.LibraryManagementSystem.controllers;
 
+import lombok.AllArgsConstructor;
 import org.oss.LibraryManagementSystem.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
+@AllArgsConstructor
 public class UserController {
-    private UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    private final UserService userService;
 }
